50135083b7adbe51af8b59b769e041d9c56af762
Serge Mugisha N. <s.nshimiyimana@bk.rw>
2024-10-05 10:27:39 +0200

    How to apply active class conditionally

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,10 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { User } from './user.model';
 
-interface User {
-  id: string;
-  name: string;
-  avatar: string;
-}
 
 @Component({
   selector: 'app-user',
@@ -14,6 +10,7 @@ interface User {
 })
 export class UserComponent {
   @Input({ required: true }) user!: User;
+  @Input({required:true}) selected!:boolean
   @Output() select = new EventEmitter<string>();
 
   get imagePath() {
